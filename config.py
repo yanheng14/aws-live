11d293139ec4fe090d8b6148b3176b0429c3c36c
yanheng14 <yanheng14@gmail.com>
2025-12-01 10:59:11 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "yh314.c5vxeamaghbb.us-east-1.rds.amazonaws.com"
+customuser = "yh314"
+custompass = "12345678"
+customdb = "yhdb"
+custombucket = "yh314-bucket"
+customregion = "us-east-1"
+
